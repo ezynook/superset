ad395d06a7c24acbef9be7135de6d163cf973648
pasit.dev <90300159+ezynook@users.noreply.github.com>
2023-07-19 10:54:57 +0700

    Add files via upload

--- SDK-Dashboard/Python/app.py
+++ SDK-Dashboard/Python/app.py
@@ -3,13 +3,15 @@ import json
 import requests
 import asyncio
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 HOST = "http://192.168.10.47:8088"
-DASHBOARD = "6e96c9aa-95f9-47f8-8046-fa764f94faaf"
-USERNAME = "test"
-PASSWORD = "test"
+DASHBOARD = "7af45060-8d1e-487f-9781-7855f06e6871"
+USERNAME = "nook"
+PASSWORD = "nook"
 
 def guest_token(key: int):
     url = "http://192.168.10.47:8088/api/v1/security/guest_token/"
@@ -47,6 +49,41 @@ def login_token():
     data = guest_token(response.json()['access_token'])
     return data
 
+@app.route('/get_token', methods=["GET"])
+def getresult():
+    dashboard_id = request.args.get('dashboard_id')
+    url = "http://192.168.10.47:8088/api/v1/security/login"
+    payload = json.dumps({
+        "password": "tang",
+        "provider": "db",
+        "refresh": True,
+        "username": "tang"
+    })
+    headers = {'Content-Type': 'application/json'}
+    response = requests.request("POST", url, headers=headers, data=payload)
+    
+    url = "http://192.168.10.47:8088/api/v1/security/guest_token/"
+    payload = json.dumps({
+        "resources": [
+            {
+            "id": dashboard_id,
+            "type": "dashboard"
+            }
+        ],
+        "rls": [],
+        "user": {
+            "first_name": "tang",
+            "last_name": "tang",
+            "username": "tang"
+        }
+    })
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + response.json()['access_token']
+    }
+    response = requests.request("POST", url, headers=headers, data=payload)
+    return response.json()['token']
+
 @app.route('/')
 def index():
     call_api = login_token()
@@ -54,4 +91,4 @@ def index():
     return render_template("index.html", data=pack_val)
     
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=3000, debug=True)
