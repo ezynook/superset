01323e2ac9b4a0031048ecac3d4d476dc80965a4
Pasit Y <ezynook@gmail.com>
2023-07-19 14:26:40 +0700

    [*] at: 20230719 14:26:40

--- sdk47.sh
+++ sdk47.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-scp SDK-Dashboard/Python/app.py root@192.168.10.47:/root/superset/sdk
+scp SDK-Dashboard/Python/* root@192.168.10.47:/root/superset/sdk/
+
+ssh root@192.168.10.47 "pm2 restart 2"
